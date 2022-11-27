d116112ff3fb13b1b13371cbd461e179e7c907c5
mathiasbln <mathias.balian92@gmail.com>
2022-11-27 22:48:24 +0900

    Added iteration over the words and comparison between counter of subjects

--- Folder_Sorter.py
+++ Folder_Sorter.py
@@ -7,12 +7,21 @@ f = open("Dataset_Topics.txt", "r")
 
 # We create a dictionary where the key is a school subject
 # and the value is the list of the words related to this subject
-dataset = {"biology": list(dict.fromkeys(f.readline().split(";"))),
-           "compsci": list(dict.fromkeys(f.readline().split(";"))),
-           "physics": list(dict.fromkeys(f.readline().split(";"))),
-           "chemistry": list(dict.fromkeys(f.readline().split(";"))),
-           "philosophy": list(dict.fromkeys(f.readline().split(";")))}
+dataset = {"biology": [list(dict.fromkeys(f.readline().split(";"))), 0],
+           "compsci": [list(dict.fromkeys(f.readline().split(";"))), 0],
+           "physics": [list(dict.fromkeys(f.readline().split(";"))), 0],
+           "chemistry": [list(dict.fromkeys(f.readline().split(";"))), 0],
+           "mathematics": [list(dict.fromkeys(f.readline().split(";"))), 0],
+           "philosophy": [list(dict.fromkeys(f.readline().split(";"))), 0]}
 f.close()
+
+# Counters for each school subject possible
+biology_counter = 0
+compsci_counter = 0
+physics_counter = 0
+chemistry_counter = 0
+philosophy_counter = 0
+total_counter = 0
 
 # We open a file dialog for the user
 tkinter.Tk().withdraw()
@@ -20,9 +29,27 @@ folder_path = filedialog.askdirectory()
 
 try:
     for filename in os.listdir(folder_path):
+        total_counter = 0
         file = os.path.join(folder_path, filename)
         if os.path.isfile(file):
-            file_extension = os.path.splitext(file)[1]
-            print(file_extension)
+            text = []
+            if os.path.splitext(file)[1] == ".pdf":  # If the file is a pdf file
+                text = textfrompdf(file)
+            elif os.path.splitext(file)[1] == ".doc" or os.path.splitext(file)[1] == ".docx":  # If the file is a docx or doc
+                text = textfromword(file)
+
+            for word in text:
+                word = word.lower()
+                total_counter += 1
+                for subject in dataset:
+                    if dataset[subject][0].count(word) > 0:
+                        dataset[subject][1] += 1
+
+        else:
+            print("The file", file, "is not supported.")
+
 except:
     print("File not provided or not found")
+
+for subject in dataset:
+    print(subject, dataset[subject][1])
