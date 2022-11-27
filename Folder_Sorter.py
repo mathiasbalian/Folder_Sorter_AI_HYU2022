d3967f0d651ef1c465fcef25a3e03b8b01d1c7c7
mathiasbln <mathias.balian92@gmail.com>
2022-11-27 16:42:00 +0900

    Added file dialog and file extension check

--- Folder_Sorter.py
+++ Folder_Sorter.py
@@ -1,3 +1,8 @@
+import os
+import tkinter
+from tkinter import filedialog
+from Util import textfrompdf
+
 f = open("Dataset_Topics.txt", "r")
 
 # We create a dictionary where the key is a school subject
@@ -9,4 +14,15 @@ dataset = {"biology": list(dict.fromkeys(f.readline().split(";"))),
            "philosophy": list(dict.fromkeys(f.readline().split(";")))}
 f.close()
 
+# We open a file dialog for the user
+tkinter.Tk().withdraw()
+folder_path = filedialog.askdirectory()
 
+try:
+    for filename in os.listdir(folder_path):
+        file = os.path.join(folder_path, filename)
+        if os.path.isfile(file):
+            file_extension = os.path.splitext(file)[1]
+            print(file_extension)
+except:
+    print("File not provided or not found")
