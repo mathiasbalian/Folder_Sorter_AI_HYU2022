1796423e7f5e18a6502bd8c5a0da8ff68994f27a
Mathias Balian <mathias.balian92@gmail.com>
2022-12-08 00:23:08 +0900

    Created Sort_Folder.py file to call model and sort files

---
+++ BoW/Sort_Folder.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from Util import textfrompdf, textfromword
+import os
+import pickle
+import tkinter
+import shutil
+from tkinter import filedialog
+from Train_Model import cv
+
+modeload = open("ML_model", "rb")
+model = pickle.load(modeload)
+modeload.close()
+
+# We open a file dialog for the user
+folder_path = filedialog.askdirectory()
+
+try:
+    for filename in os.listdir(folder_path):
+        file = os.path.join(folder_path, filename)
+        if os.path.isfile(file):
+            file_text = ""
+            if os.path.splitext(file)[1] == ".pdf":  # If the file is a pdf file
+                file_text = textfrompdf(file)
+            elif os.path.splitext(file)[1] == ".docx":  # If the file is a docx or doc
+                file_text = textfromword(file)
+
+            print(model.predict(cv.transform([file_text])))
+        else:
+            print("The file", file, "is not supported.")
+
+except:
+    print("File not provided or not found")
